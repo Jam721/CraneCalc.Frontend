684cbe6672ee9774601aece068dffcc4019a8fef
Jam721 <arturqweasd@yandex.ru>
2025-10-18 20:53:11 +0300

    Add PWA + deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Каталог грузов',
+        short_name: 'Грузы',
+        description: 'Каталог грузов с расчетом для башенного крана',
+        theme_color: '#000000',
+        background_color: '#121212',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/CraneCalc.Frontend/',
   server: {
     proxy: {
       '/api': {
@@ -12,5 +46,6 @@ export default defineConfig({
         secure: false,
       }
     }
-  }
+  },
+
 })
