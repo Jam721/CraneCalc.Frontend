4ca1ecf399b36b97cd1b0ab5f794068343c0a5cb
Jam721 <arturqweasd@yandex.ru>
2025-10-18 21:00:16 +0300

    Add PWA + deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'], // оставили только необходимые
       manifest: {
         name: 'Каталог грузов',
         short_name: 'Грузы',
@@ -14,8 +15,8 @@ export default defineConfig({
         theme_color: '#000000',
         background_color: '#121212',
         display: 'standalone',
-        scope: '/',
-        start_url: '/',
+        scope: '/CraneCalc.Frontend/',
+        start_url: '/CraneCalc.Frontend/',
         icons: [
           {
             src: '/pwa-192x192.png',
@@ -26,12 +27,6 @@ export default defineConfig({
             src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
           },
         ],
       },
@@ -44,8 +39,7 @@ export default defineConfig({
         target: 'http://localhost:5069',
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
-
-})
+});
